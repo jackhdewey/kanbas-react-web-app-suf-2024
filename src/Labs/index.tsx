8f776caeee61bd4b5a4e800b9d9030a83ce2027a
jackhdewey <jackhdewey@gmail.com>
2024-05-20 09:38:21 -0400

    Beginning lab 1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,7 +2,8 @@ import Lab1 from "./Lab1";
 
 export default function Labs() {
     return(
-        <div>
+        <div id="wd-labs">
+            <h1>Labs</h1>
             <Lab1 />
         </div>
     );
