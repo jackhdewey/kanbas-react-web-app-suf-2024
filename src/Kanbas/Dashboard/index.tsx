da6843a640b34af4444201049b5532d0bd0c820a
jackhdewey <jackhdewey@gmail.com>
2024-05-22 18:40:57 -0400

    Adding more courses

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -6,19 +6,71 @@ export default function Dashboard() {
         <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
 
         <div id="wd-dashboard-courses">
-          <div className="wd-dashboard-course">
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-
-              <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
-              <p className="wd-dashboard-course-title">Full Stack software developer</p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
-              
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
             </div>
-          </div>
-          <div className="wd-dashboard-course"> ... </div>
-          <div className="wd-dashboard-course"> ... </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
+            <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">CS1234 React JS</a>
+                    <p className="wd-dashboard-course-title">Full Stack software developer</p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+            </div>
+
         </div>
-      </div>
-  );}
+    </div>
+    );
+}
   
