8d6c67c76a7cfa7ffd76bb2ba49deabf49eb09da
jackhdewey <jackhdewey@gmail.com>
2024-05-26 11:15:37 -0400

    Assignment editor improvements

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -8,7 +8,7 @@ export default function AssignmentEditor() {
             </label>
             <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
 
-            <textarea id="wd-description" cols={50} rows={10}>
+            <textarea id="wd-description" cols={50} rows={9}>
                 The assignment is available online. Submit a link to the landing page of your web application running on Netlify. The landing page should include the following: Your full name and section, Links to each of the lab assignments, Link the the Kanbas application, Links to all relevant source code repositories. The Kanbas application should include a link to navigate back to the landing page.
             </textarea><br /><br />
 
