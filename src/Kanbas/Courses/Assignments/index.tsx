49cc4c0823d9ffaa819519ed8c753960a523a485
jackhdewey <jackhdewey@gmail.com>
2024-05-26 09:54:00 -0400

    Assignment page improvements

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -11,18 +11,18 @@ export default function Assignments() {
             <ul id="wd-assignment-list">
                 
                 <li className="wd-assignment-list-item">
-                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A1 - ENV + HTML</a>
+                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A1 - ENV + HTML</a><br />
                     Multiple Modules | <b>Not available until</b> May 6 at 12:00am | <b>Due</b> May 13 at 11:59pm | 100 pts
                 </li>
 
                 <li className="wd-assignment-list-item">
                     {/* Complete On Your Own */}
-                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A2 - CSS + BOOTSTRAP</a>
+                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A2 - CSS + BOOTSTRAP</a><br />
                     Multiple Modules | <b>Not available until</b> May 13 at 12:00am | <b>Due</b> May 20 at 11:59pm | 100 pts
                 </li>
 
                 <li className="wd-assignment-list-item">
-                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A3 - JAVASCRIPT + REACT</a>
+                    <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">A3 - JAVASCRIPT + REACT</a><br />
                     Multiple Modules | <b>Not available until</b> May 20 at 12:00am | <b>Due</b> May 27 at 11:59pm | 100 pts
                 </li>
             </ul>
