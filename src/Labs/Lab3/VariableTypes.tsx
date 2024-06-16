0517f2342468dbb0d5a7128ae0cddd5bce324902
jackhdewey <jackhdewey@gmail.com>
2024-06-16 09:59:46 -0400

    Lab 3 complete

---
+++ src/Labs/Lab3/VariableTypes.tsx
@@ -0,0 +1,20 @@
+export default function VariableTypes() {
+    let numberVariable = 123;
+    let floatingPointNumber = 234.345;
+    let stringVariable = 'Hello World!'
+    let booleanVariable = true;
+    let isNumber = typeof numberVariable;
+    let isString = typeof stringVariable;
+    let isBoolean = typeof booleanVariable;
+    return (
+        <div>
+            numberVariable = { numberVariable }<br/>
+            floatingPointNumber = { floatingPointNumber }<br/>
+            stringVariable = { stringVariable }<br/>
+            booleanVariable = { booleanVariable + "" }<br/>
+            isNumber = { isNumber }<br/>
+            isString = { isString }<br/>
+            isBoolean = { isBoolean }<hr/>
+        </div>    
+    );
+}
