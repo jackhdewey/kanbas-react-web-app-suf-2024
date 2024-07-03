49e3ece29df33c2ad3c88dfdaa43413d48bed079
jackhdewey <jackhdewey@gmail.com>
2024-07-03 17:06:02 -0400

    Lab4 reformatting

--- src/Labs/Lab4/DateStateVariable.tsx
+++ src/Labs/Lab4/DateStateVariable.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 export default function DateStateVariable() {
     const [startDate, setStartDate] = useState(new Date());
+
     const dateObjectToHtmlDateString = (date: Date) => {
         return `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? 0 : ""}${date.getMonth() + 1}-${date.getDate() + 1 < 10 ? 0 : ""}${date.getDate() + 1}`;
     };
+    
     return (
         <div id="wd-date-state-variables">
             <h2>Date State Variables</h2>
