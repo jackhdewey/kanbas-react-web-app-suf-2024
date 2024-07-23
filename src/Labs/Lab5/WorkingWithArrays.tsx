bffec12011734461d74f1d59702ab68b1500ac82
jackhdewey <jackhdewey@gmail.com>
2024-07-23 15:06:58 -0400

    Lab5 updates

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -22,7 +22,8 @@ export default function WorkingWithArrays() {
             <h4>Retrieving an Item from an Array by ID</h4>
             <a id="wd-retrieve-todo-by-id" className="btn btn-primary float-end" 
                 href={`${API}/${todo.id}`}> Get Todo by ID </a>
-            <input id="wd-todo-id" value={todo.id} className="form-control w-50"
+            <input id="wd-todo-id" className="form-control w-50"
+                value={todo.id} 
                 onChange={(e) => setTodo({ ...todo, id: e.target.value })} />
             <hr />
 
@@ -37,8 +38,10 @@ export default function WorkingWithArrays() {
             <h3>Deleting from an Array</h3>
             <a id="wd-retrieve-completed-todos" className="btn btn-primary float-end" 
                 href={`${API}/${todo.id}/delete`}> Delete Todo with ID = {todo.id} </a>
-            <input value={todo.id} onChange={(e) => setTodo({ ...todo, id: e.target.value })}
-                    className="form-control w-50"/><hr/>
+            <input className="form-control w-50"
+                value={todo.id} 
+                onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
+            <hr/>
 
             <h3>Updating an Item in an Array</h3>
             <a href={`${API}/${todo.id}/title/${todo.title}`} 
@@ -52,16 +55,18 @@ export default function WorkingWithArrays() {
             <a href={`${API}/${todo.id}/description/${todo.description}`} 
                 className="btn btn-primary float-end">Describe Todo</a>
             <input value={todo.id} className="form-control w-25 float-start me-2"
-                    onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
+                onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
             <input value={todo.description} className="form-control w-50 float-start"
-                    onChange={(e) => setTodo({ ...todo, description: e.target.value }) }/>
+                onChange={(e) => setTodo({ ...todo, description: e.target.value }) }/>
             <br /><br />
 
             <a href={`${API}/${todo.id}/completed/${todo.completed}`} className="btn btn-primary float-end">Complete Todo</a>
-            <input value={todo.id} className="form-control w-25 float-start me-2"
-                    onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
-            <input checked={todo.completed} type="checkbox" className="form-check-input float-start"
-                    onChange={(e) => setTodo({ ...todo, completed: e.target.checked }) }/>
+            <input className="form-control w-25 float-start me-2"
+                value={todo.id} 
+                onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
+            <input type="checkbox" className="form-check-input float-start"
+                checked={todo.completed} 
+                onChange={(e) => setTodo({ ...todo, completed: e.target.checked }) }/>
             <br /><br /><hr />
 
         </div>
